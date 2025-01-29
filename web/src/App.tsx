8c5ad70760ca9a034e9ce1e3d0ed47bcab0074a5
Colin Larson <9647933+ColinLarsonCA@users.noreply.github.com>
2025-01-29 00:04:14 -0600

    nest list view in Home page, prep for specific collab page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,42 +1,9 @@
-import { useEffect, useState } from "react";
-import { api } from "./api/api";
-import { AppShell, Burger, Grid, Space, Text, TextInput } from "@mantine/core";
+import { AppShell, Burger } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { Collab } from "./pb/collabcafe";
-import { CollabCard } from "./components/CollabCard";
+import { Home } from "./pages/Home";
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
-  const [collabs, setCollabs] = useState<Collab[]>([]);
-  const [error, setError] = useState("");
-  const [search, setSearch] = useState("");
-
-  const listCollabs = () => {
-    api.CollabCafe.listCollabs({ language: "en" })
-      .then((response) => {
-        setCollabs(response.collabs);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  };
-  useEffect(() => {
-    listCollabs();
-  }, []);
-
-  useEffect(() => {
-    if (search === "") {
-      listCollabs();
-    } else {
-      api.CollabCafe.searchCollabs({ language: "en", query: search })
-        .then((response) => {
-          setCollabs(response.collabs);
-        })
-        .catch((error) => {
-          setError(error.message);
-        });
-    }
-  }, [search]);
 
   return (
     <AppShell
@@ -54,29 +21,7 @@ function App() {
       </AppShell.Header>
       <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
       <AppShell.Main>
-        <TextInput
-          placeholder="Search"
-          onBlur={(event) => {
-            setSearch(event.currentTarget.value);
-          }}
-          onKeyUp={(event) => {
-            if (event.key === "Enter") {
-              setSearch(event.currentTarget.value);
-            }
-            if (event.key === "Backspace" && event.currentTarget.value === "") {
-              setSearch("");
-            }
-          }}
-        />
-        {error && <Text c="red">{error}</Text>}
-        <Space h="md" />
-        <Grid>
-          {(collabs || []).map((collab) => (
-            <Grid.Col span={{ base: 12, md: 6, lg: 4 }}>
-              <CollabCard collab={collab} />
-            </Grid.Col>
-          ))}
-        </Grid>
+        <Home />
       </AppShell.Main>
     </AppShell>
   );
---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { api } from "../api/api";
+import { Container, Grid, Space, Text, TextInput } from "@mantine/core";
+import { Collab } from "../pb/collabcafe";
+import { CollabCard } from "../components/CollabCard";
+
+export function Home() {
+  const [collabs, setCollabs] = useState<Collab[]>([]);
+  const [error, setError] = useState("");
+  const [search, setSearch] = useState("");
+  const listCollabs = () => {
+    api.CollabCafe.listCollabs({ language: "en" })
+      .then((response) => {
+        setCollabs(response.collabs);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+  useEffect(() => {
+    listCollabs();
+  }, []);
+  useEffect(() => {
+    if (search === "") {
+      listCollabs();
+    } else {
+      api.CollabCafe.searchCollabs({ language: "en", query: search })
+        .then((response) => {
+          setCollabs(response.collabs);
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    }
+  }, [search]);
+  return (
+    <Container>
+      <TextInput
+        placeholder="Search"
+        onBlur={(event) => {
+          setSearch(event.currentTarget.value);
+        }}
+        onKeyUp={(event) => {
+          if (event.key === "Enter") {
+            setSearch(event.currentTarget.value);
+          }
+          if (event.key === "Backspace" && event.currentTarget.value === "") {
+            setSearch("");
+          }
+        }}
+      />
+      {error && <Text c="red">{error}</Text>}
+      <Space h="md" />
+      <Grid>
+        {(collabs || []).map((collab) => (
+          <Grid.Col span={{ base: 12, md: 6, lg: 4 }}>
+            <CollabCard collab={collab} />
+          </Grid.Col>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
